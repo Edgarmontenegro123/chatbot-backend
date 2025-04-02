6186345162ba16bf896380760ab59567037fc43c
Edgar <edgarmontenegro321@gmail.com>
2025-04-02 03:57:56 -0300

    🚑 Repair CORS issue and connect front with back

--- main.go
+++ main.go
@@ -6,10 +6,29 @@ import (
 	"net/http"
 )
 
+// Middleware para habilitar CORS
+func enableCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding")
+
+		// Manejar preFlight request
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	mux := http.NewServeMux()
 	routes.RegisterRoutes(mux)
 
+	// Aplicar Middleware CORS
+	handler := enableCors(mux)
+
 	log.Println("Servidor corriendo en http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
